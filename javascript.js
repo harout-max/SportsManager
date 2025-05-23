77bff6240bf9dd1d1b63e37b4568d12ef149dd1e
Harout Karakeshishian <69994135+harout-max@users.noreply.github.com>
2025-05-23 16:25:59 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// This script can be used for future enhancements or interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is loaded and ready to use!");
+    // You can add more interactive features here in the future
+});
