f567715445b4f5052d6240ff5f5075bfb1e7f3b6
Harout Karakeshishian <69994135+harout-max@users.noreply.github.com>
2025-05-23 16:25:33 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,5 +0,0 @@
-// This script can be used for future enhancements or interactivity
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript is loaded and ready to use!");
-    // You can add more interactive features here in the future
-});
